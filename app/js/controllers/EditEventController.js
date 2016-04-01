7f7fcf9b4a1c5644894fec03b585404179a0be79
Dan Clarke <dan.clarke@brainjuicer.com>
2016-04-01 16:02:41 +0100

    implemented an edit page using two way binding

---
+++ app/js/controllers/EditEventController.js
@@ -0,0 +1,13 @@
+'use strict';
+
+eventsApp.controller('EditEventController',
+    function($scope) {
+        $scope.saveEvent = function(event) {
+            window.alert('event ' + event.name + ' saved!');
+        }
+
+        $scope.cancelEvent = function () {
+            window.location = "/EventDetails.html";
+        }
+    }
+);

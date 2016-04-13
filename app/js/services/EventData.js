bd272dd013d82c99841c76c481cc747384658d6d
Dan Clarke <dan.clarke@brainjuicer.com>
2016-04-13 17:27:02 +0100

    hooked up to a webapi that read writes event data from a file

--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -5,7 +5,7 @@ eventsApp.factory('eventData' , function($http, $log){
     return{
         getEvent: function(successCallback) {
             $http({method: 'GET', url: '/data/event/1'}).success(function(data, status, headers, config) {
-                successcb(data);
+                successCallback(data);
             }).error( function (data, status, headers, config) {
                $log.warn(data, status, headers, config);
             });
